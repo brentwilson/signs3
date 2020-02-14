89b2d40ded102b8145933a21852a85716374fbab
Brent Wilson <brent@prmconnect.com>
2020-02-14 14:00:00 -0800

    initial commit

---
+++ forum2020/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Forum2020Config(AppConfig):
+    name = 'forum2020'
---
+++ knowledge2020/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Knowledge2020Config(AppConfig):
+    name = 'knowledge2020'
